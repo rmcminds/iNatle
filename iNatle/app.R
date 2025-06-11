8b196db7a7ec20116ea462a2394c1e19370542dd
Ryan McMinds <r.mcminds@gmail.com>
2025-06-11 13:20:31 -0700

    Update app.R

--- iNatle/app.R
+++ iNatle/app.R
@@ -660,7 +660,7 @@ server <- function(input, output, session) {
       )
 
       # update url in browser to reflect chosen parameters
-      session$sendCustomMessage("update_url", as.list(setNames(URLencode(allparams),names(allparams))))
+      session$sendCustomMessage("update_url", as.list(allparams))
 
       if(!r$is_random) {
 
